9d2b64a6a4f0312a644d49f453a2bc25e25b0258
Jenny Lin <misslinyang91@gmail.com>
2021-08-27 10:02:00 +1000

    added converting formulas and tofix decimals

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const celciusInput = document.querySelector("#celcius");
-const farenheitInput = document.querySelector("#farenheit");
+const fahrenheitInput = document.querySelector("#fahrenheit");
 const kelvinInput = document.querySelector("#kelvin");
 const tempInputs = document.querySelectorAll("input");
-const h1 = document.querySelector(".title");
 
 tempInputs.forEach(function (input) {
   input.addEventListener("input", function (e) {
@@ -10,9 +9,39 @@ tempInputs.forEach(function (input) {
 
     let tempValue = parseInt(e.target.value);
     let inputName = e.target.name;
-  });
-});
 
-h1.addEventListener("click", function (e) {
-  console.log(e);
+    if (e.target.value === "") {
+      celciusInput.value = null;
+      fahrenheitInput.value = null;
+      kelvinInput.value = null;
+
+      return;
+    }
+
+    if (inputName === "celcius") {
+      //celcius to fahrenheit
+      let fahrenheitValue = tempValue * 1.8 + 32;
+      fahrenheitInput.value = fahrenheitValue.toFixed(2);
+      //celcius to kelvin
+      let kelvinValue = tempValue + 273.15;
+      kelvinInput.value = kelvinValue.toFixed(2);
+    } else if (inputName === "fahrenheit") {
+      //fahrenheit to celcius
+
+      let celciusValue = (tempValue - 32) / 1.8;
+      celciusInput.value = celciusValue.toFixed(2);
+      //fahrenheit to kelvin
+
+      let kelvinValue = (tempValue - 32) / 1.8 + 273.15;
+      kelvinInput.value = kelvinValue.toFixed(2);
+    } else if (inputName === "kelvin") {
+      //kelvin to celcius
+      let celciusValue = tempValue - 273.15;
+      celciusInput.value = celciusValue.toFixed(2);
+
+      //kelvin to fahrenheit
+      let fahrenheitValue = (tempValue - 273.15) * 1.8 + 32;
+      fahrenheitInput.value = fahrenheitValue.toFixed(2);
+    }
+  });
 });
